457fd1ad8ea14a9f80e3946234ba25ad612e590f
dmytroKarataiev <karataev.dima@gmail.com>
2015-12-14 22:37:40 -0800

    fix: add if/else flow statement

--- app/src/main/java/karataiev/dmytro/popularmovies/MainActivityFragment.java
+++ app/src/main/java/karataiev/dmytro/popularmovies/MainActivityFragment.java
@@ -1,7 +1,7 @@
 package karataiev.dmytro.popularmovies;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -41,10 +41,14 @@ public class MainActivityFragment extends Fragment {
             Log.v(LOG_TAG, "error" + e2);
         }
 
-        movieAdapter = new MovieObjectAdapter(getActivity(), Arrays.asList(movies));
-
-        GridView gridView = (GridView) rootview.findViewById(R.id.movies_grid);
-        gridView.setAdapter(movieAdapter);
+        if (movies != null) {
+            movieAdapter = new MovieObjectAdapter(getActivity(), Arrays.asList(movies));
+            GridView gridView = (GridView) rootview.findViewById(R.id.movies_grid);
+            gridView.setAdapter(movieAdapter);
+        }
+        else {
+            movieAdapter = null;
+        }
 
         return rootview;
     }
