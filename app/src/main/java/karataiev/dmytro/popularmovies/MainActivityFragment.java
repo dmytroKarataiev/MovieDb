918919b5171ff9922c89e6c1434340269d24ee93
dmytroKarataiev <karataev.dima@gmail.com>
2015-12-17 19:39:54 -0800

    add: OkHttpClient library

--- app/src/main/java/karataiev/dmytro/popularmovies/MainActivityFragment.java
+++ app/src/main/java/karataiev/dmytro/popularmovies/MainActivityFragment.java
@@ -19,11 +19,11 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 
-import java.io.BufferedReader;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
@@ -288,8 +288,8 @@ public class MainActivityFragment extends Fragment {
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
 
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
+            OkHttpClient client = new OkHttpClient();
+            Response responses = null;
 
             // Will contain the raw JSON response as a string.
             String movieJsonStr;
@@ -328,33 +328,20 @@ public class MainActivityFragment extends Fragment {
                 URL url = new URL(builtUri.toString());
 
                 // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
 
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuilder buffer = new StringBuilder();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
 
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    line += "\n";
-                    buffer.append(line);
+                try {
+                    responses = client.newCall(request).execute();
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, "Error ", e);
                 }
 
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                movieJsonStr = buffer.toString();
+                movieJsonStr = responses.body().string();
+
+                responses.body().close();
 
                 return Utility.getMoviesGSON(mContext, movieJsonStr);
 
@@ -365,18 +352,9 @@ public class MainActivityFragment extends Fragment {
                 // If the code didn't successfully get the weather data, there's no point in attempting
                 // to parse it.
                 return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
+
             }
+
         }
 
         @Override
