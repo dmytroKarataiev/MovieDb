e5598332cccb237ac8fb5d42278b5f4aa787e369
dmytroKarataiev <karataev.dima@gmail.com>
2015-12-20 20:47:55 -0800

    fix: favorite buttons

--- app/src/main/java/karataiev/dmytro/popularmovies/DetailActivity.java
+++ app/src/main/java/karataiev/dmytro/popularmovies/DetailActivity.java
@@ -2,8 +2,6 @@ package karataiev.dmytro.popularmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 /**
  * Class to start DetailActivityFragment
@@ -22,29 +20,5 @@ public class DetailActivity extends AppCompatActivity {
                     .add(R.id.container, new DetailFragment())
                     .commit();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
